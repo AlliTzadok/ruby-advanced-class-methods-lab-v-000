3cbc8b210253f25b42c51611e2b5442f5983b18d
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 00:50:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,11 @@ class Song
   def self.find_by_name(name)
     self.all.detect {|s| s.name == name}
   end
-    
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
 
 
 
